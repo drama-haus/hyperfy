b82f5c01389b8e0a35c1d4c2e7780d76fc032275
peezy <itspeezy@protonmail.com>
2025-03-25 17:10:58 -0300

    world_spawnEntity v1.0.4

--- src/core/createServerWorld.js
+++ src/core/createServerWorld.js
@@ -3,11 +3,12 @@ import { World } from './World'
 import { Server } from './systems/Server'
 import { ServerNetwork } from './systems/ServerNetwork'
 import { ServerLoader } from './systems/ServerLoader'
-
+import { EntitySpawner } from './systems/EntitySpawner'
 export function createServerWorld() {
   const world = new World()
   world.register('server', Server)
   world.register('network', ServerNetwork)
   world.register('loader', ServerLoader)
+  world.register('entitySpawner', EntitySpawner)
   return world
 }
